38fea03e43b2db90ed6e833780323aa6421446c2
Vicky Kumar <imvickykumar999@gmail.com>
2024-06-16 10:15:28 +0530

    zipp lived

---
+++ vickipedia/vicksapp/forms.py
@@ -0,0 +1,9 @@
+# vicksapp/forms.py
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
---
+++ vickipedia/vicksapp/urls.py
@@ -0,0 +1,13 @@
+# vicksapp/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_create, name='post_create'),
+    path('post/<int:pk>/edit/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:pk>/confirm_delete/', views.post_confirm_delete, name='post_confirm_delete'),
+]
